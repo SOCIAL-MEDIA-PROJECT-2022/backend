c5c0c2f413b9586dbf9eb07d3758042c005e1ef8
Gordon Ramsbottom III <109102092+GRamsbottom@users.noreply.github.com>
2022-09-14 12:29:45 -0700

    follow service + follow model

---
+++ bin/src/main/resources/data.sql
@@ -0,0 +1,45 @@
+INSERT INTO users (id, email, password, first_name, last_name) VALUES (
+    1,
+    'testuser@gmail.com',
+    'password',
+    'Test',
+    'User'
+    
+),
+
+(2,
+'testuser2Qgamil.com',
+'password',
+'test',
+'User2'
+);
+
+INSERT INTO posts (id, text, image_url, author_id) VALUES (
+    10000,
+    'The classic',
+    'https://i.imgur.com/fhgzVEt.jpeg',
+    1
+),
+(
+    10001,
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    '',
+    1
+),
+(10002,
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    '',
+    2
+); 
+
+/*INSERT INTO following (user_id, following_id) VALUES(
+	1,2);*/
+
+
+
+
+
+
+
+
+
