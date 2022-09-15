e4e194fbff80a33c161b9f6b0319619cc80f1a96
GorillaStixs <berly.kem@gmail.com>
2022-09-15 12:51:50 -0400

    Custom Likes Exception

---
+++ src/main/java/com/revature/exceptions/LikesException.java
@@ -0,0 +1,15 @@
+package com.revature.exceptions;
+
+public class LikesException extends RuntimeException{
+
+
+    public LikesException(  ){
+        super("You already liked this post.");
+    }
+
+    public LikesException( String message)
+    {
+        super(message);
+
+    }
+}
