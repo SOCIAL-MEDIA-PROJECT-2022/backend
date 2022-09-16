303676377321b9e6e35a58150356ccebc3744401
jamesWalkr <walkerjames9981@gmail.com>
2022-09-16 09:05:24 -0700

    search-feature testing

--- src/main/java/com/revature/controllers/SearchController.java
+++ src/main/java/com/revature/controllers/SearchController.java
@@ -1,5 +1,42 @@
 package com.revature.controllers;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.annotations.Authorized;
+import com.revature.models.User;
+import com.revature.services.SearchService;
+
+
+@RestController
+@RequestMapping("/post")
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 public class SearchController {
+	
+	private final SearchService searchService;
+	
+	public SearchController(SearchService searchService) {
+		this.searchService = searchService;
+	}
+	
+	 @Authorized
+	 @PostMapping
+	 public ResponseEntity<List<User>> search(@RequestBody String  body){
+		 
+		 return ResponseEntity.ok(this.searchService.search(body));
+		 
+		 
+	 }
+	 
+	
+	
+	
+	
 
 }
