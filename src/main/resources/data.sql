ce9dc844a9fcdeac9b72f533a0c4c6d789602111
JD Megargel <109110533+MegaJam750@users.noreply.github.com>
2022-09-16 11:10:00 -0700

    started Profile models page

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,10 +8,10 @@ INSERT INTO users (id, email, password, first_name, last_name) VALUES (
 
 INSERT INTO users (id, email, password, first_name, last_name) VALUES (
     2,
-    'jay@email.com',
-    'password',
-    'Jaylen',
-    'Byrd'
+    'j@io.com',
+    '123',
+    'Jammin',
+    'Yammin'
 );
 
 INSERT INTO posts (id, text, image_url, author_id) VALUES (
