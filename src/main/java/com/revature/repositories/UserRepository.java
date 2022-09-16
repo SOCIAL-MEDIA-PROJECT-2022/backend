5b92373849fd4e3c7e4726df287e3e5a7a21c0d2
jamesWalkr <walkerjames9981@gmail.com>
2022-09-16 11:23:40 -0700

    fixing search-feature

--- src/main/java/com/revature/repositories/UserRepository.java
+++ src/main/java/com/revature/repositories/UserRepository.java
@@ -2,12 +2,24 @@ package com.revature.repositories;
 
 import com.revature.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByEmailAndPassword(String email, String password);
     
+
     Optional<User> findByEmail(String email);
+
+    List<User> searchByEmail(String email);
+    
+    @Query("FROM users u WHERE u.email LIKE %:name%")
+    List<User> findByEmailContains(@Param("email")String email);
+    
+    
+ 
 }
