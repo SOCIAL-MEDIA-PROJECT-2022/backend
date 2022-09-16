2dc1d941e3739ad66f8fc50f9e562b696d1fbff7
jamesWalkr <walkerjames9981@gmail.com>
2022-09-16 12:07:36 -0700

    search-feature endpoint added

--- src/main/java/com/revature/controllers/SearchController.java
+++ src/main/java/com/revature/controllers/SearchController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +17,7 @@ import com.revature.services.SearchService;
 
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/search")
 @CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 public class SearchController {
 	
@@ -26,10 +28,10 @@ public class SearchController {
 	}
 	
 	 @Authorized
-	 @PostMapping
-	 public ResponseEntity<List<User>> search(@RequestBody String  body){
+	 @GetMapping("/{val}")
+	 public ResponseEntity<List<User>> search(@PathVariable String  val){
 		 
-		 return ResponseEntity.ok(this.searchService.search(body));
+		 return ResponseEntity.ok(this.searchService.search(val));
 		 
 		 
 	 }
