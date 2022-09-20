0ddaf11567f1ceda33b9e764a2d07ba172718f1c
jamesWalkr <walkerjames9981@gmail.com>
2022-09-20 14:23:20 -0700

    updated SearchController

--- src/main/java/com/revature/controllers/SearchController.java
+++ src/main/java/com/revature/controllers/SearchController.java
@@ -1,6 +1,8 @@
 package com.revature.controllers;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -21,6 +23,9 @@ import com.revature.services.SearchService;
 @CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 public class SearchController {
 	
+	private static final Logger logger = Logger.getLogger(SearchController.class.getName());
+	private static final Level logLevel = Level.INFO;
+	
 	private final SearchService searchService;
 	
 	public SearchController(SearchService searchService) {
@@ -31,6 +36,7 @@ public class SearchController {
 	 @GetMapping("/{val}")
 	 public ResponseEntity<List<User>> search(@PathVariable String  val){
 		 
+		 logger.log(logLevel,"made it with : " + val);	 
 		 return ResponseEntity.ok(this.searchService.search(val));
 		 
 		 
