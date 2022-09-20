231ce651f0bce85821d035609ea644cef9e0512b
jaylenbyrd <jaybyrd1194@live.com>
2022-09-20 14:50:47 -0400

    able to grab user's profile

--- src/main/java/com/revature/controllers/ProfileController.java
+++ src/main/java/com/revature/controllers/ProfileController.java
@@ -1,11 +1,18 @@
 package com.revature.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.annotations.Authorized;
 import com.revature.models.Profile;
 import com.revature.models.User;
 import com.revature.services.ProfileService;
@@ -24,6 +31,13 @@ private final ProfileService profileService;
 	@PutMapping("/update")
 	public Profile updateProfile(@RequestBody Profile p) {
 		return profileService.saveOrUpdateProfile(p);
+	}
+	
+	@Authorized
+	@GetMapping("/{id}")
+	public ResponseEntity<List<Profile>> getProfile(@PathVariable int id){
+		return ResponseEntity.ok(this.profileService.getById(id));
+		
 	}
 
 }
--- src/main/java/com/revature/services/ProfileService.java
+++ src/main/java/com/revature/services/ProfileService.java
@@ -2,6 +2,10 @@ package com.revature.services;
 
 
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.revature.models.Profile;
@@ -25,5 +29,27 @@ public class ProfileService {
 			profileRepository.save(p);
 			System.out.println(p);
 			return profileRepository.getById(p.getId());
+		}
+
+
+		public List<Profile> getAll() {
+			// TODO Auto-generated method stub
+			return this.profileRepository.findAll();
+		}
+		
+		public List<Profile> getById(int id){
+			
+			List<Profile> profileList = profileRepository.findAll();
+			
+			List<Profile> matchingProfile = new LinkedList<>();
+			
+			for(Profile p: profileList) {
+				if(p.getUser().getId() == id) {
+					matchingProfile.add(p);
+				}
+				
+			}
+			
+			return matchingProfile;
 		}
 }
