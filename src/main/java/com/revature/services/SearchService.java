4b0aa86f5f17de80d42574dd43110e38d3bef6c6
Matt Myers <myersmatt1987@gmail.com>
2022-09-29 06:49:05 -0700

    simplified search

--- src/main/java/com/revature/services/SearchService.java
+++ src/main/java/com/revature/services/SearchService.java
@@ -21,19 +21,13 @@ public class SearchService {
 
     public List<SearchReturn> search(String s) {
 
-        List<User> userList = userRepository.findAll();
+        List<User> userList = userRepository.findByEmailContainingIgnoreCase(s);
 
         List<SearchReturn> matchingUser = new LinkedList<>();
         for (User u : userList) {
-            if (u.getEmail().contains(s)) {
-                matchingUser.add(new SearchReturn(u.getId(), u.getEmail(), u.getFirstName(), u.getLastName()));
-            }
+            matchingUser.add(new SearchReturn(u.getId(), u.getEmail(), u.getFirstName(), u.getLastName()));
         }
         return matchingUser;
 
-       
-
-
     }
-
 }
